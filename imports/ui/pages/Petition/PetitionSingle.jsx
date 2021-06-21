c89ceafabd289b3cdc245e12a052e1d06a2a952a
danilo-89 <danilo89abcd@gmail.com>
2021-06-21 22:48:14 +0200

    petitionSingle component changes

--- imports/ui/pages/Petition/PetitionSingle.jsx
+++ imports/ui/pages/Petition/PetitionSingle.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './PetitionSingle.css'
 
 const PetitionSingle = (props) => {
     return ( 
         <>
-        <img src={props.imageCover} alt="" />
+        <div 
+        className="cover-holder"
+        >
+            <div 
+            className="cover-holder__cover"
+            style={
+                {backgroundImage:`url(${props.imageCover})`}
+            }
+            >
+
+            </div>
+            <div
+            className="cover-holder__avatar"
+            style={
+                {backgroundImage:`url(${props.imageCover})`}
+            }
+            >
+                <div className=""></div>
+                <div className="">test</div>
+            </div>
+        </div>
+        {/* <img src={props.imageCover} alt="" /> */}
         <h2>{props.title}</h2>
         <h3>For: {props.towards}</h3>
         <p>{props.overview}</p>
