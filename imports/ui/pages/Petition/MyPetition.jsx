2f4f3ee499612f2013679ae038ec8f4a86a0df38
danilo-89 <danilo89abcd@gmail.com>
2021-07-23 21:59:39 +0200

    new changes

--- imports/ui/pages/Petition/MyPetition.jsx
+++ imports/ui/pages/Petition/MyPetition.jsx
@@ -1,10 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import Chart from 'chart.js/auto';
 import Signatures from '../../../../lib/signatures';
+import './MyPetition.css'
 
 const MyPetition = () => {
 
+
+    const [dateRange, setDateRange] = useState();
 
     const {signatures, isSignaturesLoading} = useTracker(() => {
         const handler = Meteor.subscribe('chartSignatures')
@@ -15,63 +18,160 @@ const MyPetition = () => {
         }
         const signatures = Signatures.find().fetch();
         console.log(signatures);
-        return {signatures}
+        return {signatures, isSignaturesLoading: false}
     });
 
-    // useEffect(() => {
-    //     createChart();
-    // });
+    useEffect(() => {
+        if(isSignaturesLoading==false) {
+            const checkIfChart = Chart?.instances?.length || false;
+            console.log(checkIfChart)
+            if(checkIfChart) {
+                console.log("already exists");
+            } else {
+                console.log("chart init");
+                createChart();
+            }
+        }
+    }, [isSignaturesLoading]);
 
-    // let counts = arr.reduce((acc, value) => ({
-    //     ...acc,
-    //     [value]: (acc[value] || 0) + 1
-    //  }), {});
+    const countObject = (arr) => {
+        const newArr = arr.map((item) => item.createdAt.toLocaleDateString())
+        const reduceArr = newArr.reduce((acc, value) => ({
+            ...acc,
+            [value]: (acc[value] || 0) + 1
+        }), {});
+        console.log(reduceArr);
+        return reduceArr;
+    }
 
     const createChart = () => {
-        var ctx = document.getElementById('myChart');
-        const custom = {
-            January: 10,
-            February: 20,
-            March: 19,
-            April: 20,
-            May: 10,
-            June: 109,
-        }
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var gradient = ctx.createLinearGradient(0, 0, 0, 400)
+        gradient.addColorStop(0, 'rgba(13, 150, 254, 0.7)')
+        gradient.addColorStop(0.6, 'transparent')
+
+        const custom = countObject(signatures);
+
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
                 labels: Object.keys(custom),
                 datasets: [{
-                    label: '# of Votes',
+                    label: `test`,
                     data: Object.values(custom),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                    fill: 'start',
+
+                    backgroundColor: gradient,
+                    borderColor: '#0D95FE',
+                    tension: 0.1,
+                    borderWidth: 2,
                 }]
             },
             options: {
                 scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            fontColor: "lightgray",
+                        }
+                    }],
                     y: {
                         beginAtZero: true
                     }
-                }
+                },
+                maintainAspectRatio: false,
+                legend: {
+                    display: false
+                },
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                },
             }
         });
+
+        console.log(Chart?.instances[0]?.chart?.canvas?.id || "no");
+        // CHART.js END
+
+        // updateData()
+
+
+
+
+
+
+
+
+
+        // var ctx = document.getElementById('myChart');
+        // const custom = {
+        //     January: 10,
+        //     February: 20,
+        //     March: 19,
+        //     April: 20,
+        //     May: 10,
+        //     June: 109,
+        // }
+        // var myChart = new Chart(ctx, {
+        //     type: 'bar',
+        //     data: {
+        //         labels: Object.keys(custom),
+        //         datasets: [{
+        //             label: '# of Votes',
+        //             data: Object.values(custom),
+        //             backgroundColor: [
+        //                 'rgba(255, 99, 132, 0.2)',
+        //                 'rgba(54, 162, 235, 0.2)',
+        //                 'rgba(255, 206, 86, 0.2)',
+        //                 'rgba(75, 192, 192, 0.2)',
+        //                 'rgba(153, 102, 255, 0.2)',
+        //                 'rgba(255, 159, 64, 0.2)'
+        //             ],
+        //             borderColor: [
+        //                 'rgba(255, 99, 132, 1)',
+        //                 'rgba(54, 162, 235, 1)',
+        //                 'rgba(255, 206, 86, 1)',
+        //                 'rgba(75, 192, 192, 1)',
+        //                 'rgba(153, 102, 255, 1)',
+        //                 'rgba(255, 159, 64, 1)'
+        //             ],
+        //             borderWidth: 1
+        //         }]
+        //     },
+        //     options: {
+        //         scales: {
+        //             y: {
+        //                 beginAtZero: true
+        //             }
+        //         }
+        //     }
+        // });
     }
+
+    // function updateData() {
+
+    //     Chart.helpers.each(Chart.instances, function (instance) {
+    
+    //         const chart = instance;
+    //         //   console.log(instance.chart.canvas.id)
+    
+    //         const data = OHLCV_historical.find({}, { sort: { start: 1 } }).fetch();
+    
+    //         const formatTimes = data.map((item) => new Date(item.start).toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' }))
+    
+    //         const formatValues = data.map((item) => item.close)
+    
+    //         chart.data.labels = formatTimes;
+    //         chart.data.datasets[0].data = formatValues;
+    
+    //         chart.update()
+    //     })
+    
+    // }
 
 
     return ( 
