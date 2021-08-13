b603f84998a440111386dedeb7c8424fe0f089a3
danilo-89 <danilo89abcd@gmail.com>
2021-08-13 12:33:34 +0200

    react chartjs npm installed

--- imports/ui/pages/Petition/MyPetition.jsx
+++ imports/ui/pages/Petition/MyPetition.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
+
 import Chart from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+
 import Signatures from '../../../../lib/signatures';
 import './MyPetition.css'
 import makeQR from '../../components/GenerateQR';
@@ -9,6 +12,7 @@ import Form from 'react-bootstrap/Form';
 const MyPetition = () => {
 
     const [dateRange, setDateRange] = useState();
+    const [data1, setData1] = useState();
 
     const { signatures, isSignaturesLoading } = useTracker(() => {
         const handler = Meteor.subscribe('chartSignatures')
@@ -23,16 +27,39 @@ const MyPetition = () => {
         return { signatures, isSignaturesLoading: false }
     });
 
+    const genData = () => {
+        
+        const custom = countObject(signatures);
+        
+        return {
+          labels: [...custom.keys()],
+          datasets: [
+            {
+              label: '# of Votes',
+              data: [...custom.values()],
+              fill: false,
+              backgroundColor: '#16DB93',
+              borderColor: '#16DB93',
+              tension: 0.1,
+              borderWidth: 2,
+            },
+          ],
+      }
+    };
+
     useEffect(() => {
         if (isSignaturesLoading == false) {
-            const checkIfChart = Chart?.instances?.length || false;
-            console.log(checkIfChart)
-            if (checkIfChart) {
-                console.log("already exists");
-            } else {
-                console.log("chart init");
-                createChart();
-            }
+
+            setData1(genData());
+            // const checkIfChart = Chart?.instances?.length || false;
+            // console.log(checkIfChart)
+            // if (checkIfChart) {
+            //     console.log("already exists");
+            //     updateData();
+            // } else {
+            //     console.log("chart init");
+            //     createChart();
+            // }
         }
     }, [isSignaturesLoading]);
 
@@ -59,28 +86,21 @@ const MyPetition = () => {
 
         var ctx = document.getElementById('myChart').getContext('2d');
         var gradient = ctx.createLinearGradient(0, 0, 0, 400)
-        gradient.addColorStop(0, 'rgba(13, 150, 254, 0.7)')
-        gradient.addColorStop(0.6, 'transparent')
-
-        const custom = countObject(signatures);
-        console.log("custom")
-        console.log(custom)
-
-
-        console.log([...custom.keys()])
-        console.log([...custom.values()])
+        gradient.addColorStop(0, 'rgba(22, 219, 147, 0.5)')
+        gradient.addColorStop(0.85, 'rgba(22, 219, 147, 0.4)')
+        gradient.addColorStop(1, 'rgba(22, 219, 147, 0.1)')
 
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: [...custom.keys()],
+                labels: [],
                 datasets: [{
                     label: `test`,
-                    data: [...custom.values()],
+                    data: [],
                     fill: 'start',
 
                     backgroundColor: gradient,
-                    borderColor: '#0D95FE',
+                    borderColor: '#16DB93',
                     tension: 0.1,
                     borderWidth: 2,
                 }]
@@ -88,16 +108,11 @@ const MyPetition = () => {
             options: {
                 scales: {
                     xAxes: [{
-                        display: false
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontColor: "lightgray",
+                        display: false,
+                        gridLines: {
+                            color: 'green'
                         }
-                    }],
-                    y: {
-                        beginAtZero: true
-                    }
+                    }]
                 },
                 maintainAspectRatio: false,
                 legend: {
@@ -114,13 +129,67 @@ const MyPetition = () => {
         console.log(Chart?.instances[0]?.chart?.canvas?.id || "no");
         // CHART.js END
 
-        // updateData()
+        setTimeout(() => {
+            updateData()
+        }, 5000)
 
 
 
 
 
+        // setTimeout(() => {
+        //     // CHART.js
+        //     var ctx = document.getElementById('myChart').getContext('2d');
+        //     var gradient = ctx.createLinearGradient(0, 0, 0, 400)
+        //     gradient.addColorStop(0, 'rgba(13, 150, 254, 0.7)')
+        //     gradient.addColorStop(0.6, 'transparent')
+        //     const labels = [];
+        //     var myChart = new Chart(ctx, {
+        //         type: 'line',
+        //         data: {
+        //             labels: labels,
+        //             datasets: [{
+        //                 label: `${FlowRouter.getQueryParam('coin')}/${FlowRouter.getQueryParam('stable')}`,
+        //                 data: [],
+        //                 fill: 'start',
 
+        //                 backgroundColor: gradient,
+        //                 borderColor: '#0D95FE',
+        //                 tension: 0.1,
+        //                 borderWidth: 2,
+        //             }]
+        //         },
+        //         options: {
+        //             scales: {
+        //                 xAxes: [{
+        //                     display: false
+        //                 }],
+        //                 yAxes: [{
+        //                     ticks: {
+        //                         fontColor: "lightgray",
+        //                     }
+        //                 }],
+        //                 y: {
+        //                     beginAtZero: true
+        //                 }
+        //             },
+        //             maintainAspectRatio: false,
+        //             responsive: true,
+        //             legend: {
+        //                 display: false
+        //             },
+        //             elements: {
+        //                 point: {
+        //                     radius: 0
+        //                 }
+        //             },
+        //         }
+        //     });
+        //     // CHART.js END
+
+        //     updateData()
+
+        // }, 0)
 
 
 
@@ -169,30 +238,70 @@ const MyPetition = () => {
         // });
     }
 
-    // function updateData() {
 
-    //     Chart.helpers.each(Chart.instances, function (instance) {
+    function updateData() {
 
-    //         const chart = instance;
-    //         //   console.log(instance.chart.canvas.id)
+        console.log('inside update 0')
 
-    //         const data = OHLCV_historical.find({}, { sort: { start: 1 } }).fetch();
+        console.log(Chart)
+        console.log(Chart.getChart())
+        console.log(Chart?.instances?.length)
 
-    //         const formatTimes = data.map((item) => new Date(item.start).toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' }))
+        if (Chart?.instances?.length) {
+            console.log('inside update 1')
 
-    //         const formatValues = data.map((item) => item.close)
+            Chart.helpers.each(Chart.instances, function (instance) {
 
-    //         chart.data.labels = formatTimes;
-    //         chart.data.datasets[0].data = formatValues;
 
-    //         chart.update()
-    //     })
+                console.log('inside update 2')
+                const chart = instance;
 
-    // }
+                const custom = countObject(signatures);
+                chart.data.labels = [...custom.keys()];
+                chart.data.datasets[0].data = [...custom.values()];
+
+                chart.update()
+
+            })
+        }
+
+    }
+
+
+    // var gradient = ctx.createLinearGradient(0, 0, 0, 400)
+    // gradient.addColorStop(0, 'rgba(22, 219, 147, 0.5)')
+    // gradient.addColorStop(0.85, 'rgba(22, 219, 147, 0.4)')
+    // gradient.addColorStop(1, 'rgba(22, 219, 147, 0.1)')
+
+  
+      
+      const options1 = {
+        scales: {
+            xAxes: [{
+                display: false,
+                gridLines: {
+                    color: 'green'
+                }
+            }]
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+    }
 
 
     return (
         <div className="container">
+
+
+
+        <Line data={data1} options={options1} />
 
             {
                 isSignaturesLoading ?
