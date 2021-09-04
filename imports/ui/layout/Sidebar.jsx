d9e6eb39382c9cef42975f817fc8aa5ebde9071d
danilo-89 <danilo89abcd@gmail.com>
2021-09-04 22:52:44 +0200

    new changes

--- imports/ui/layout/Sidebar.jsx
+++ imports/ui/layout/Sidebar.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import './Sidebar.css'
+import './Sidebar.css';
+import { UserContext } from '../../context/userContext';
+import { Link, NavLink } from 'react-router-dom';
+import helpers from '../components/GlobalHelpers';
 
 const Sidebar = ({toggleDrawer, state}) => {
 
+    const { profileData } = useContext(UserContext);
 
     return (
         <SwipeableDrawer
@@ -14,15 +18,23 @@ const Sidebar = ({toggleDrawer, state}) => {
         >
             <div className="side-menu">
                 <ul>
-                    <li className="side-menu__title"><span>petitions</span></li>
+                    <li className="side-menu__title pl-0">
+                        {Meteor.user() ?
+                        <>
+                        <NavLink exact to="/profile" className="nav-link profile-small" activeClassName="circle-active">
+                            <img className="nav-avatar" src={profileData?.picture ? helpers.getImgUrlById(profileData?.picture || '') : '/abstract-user-flat-4.svg'} alt="user profile picture" />
+                        </NavLink>
+                        {Meteor.user().username}
+                        </>
+                        :
+                        <span className="pl-3">Sign up</span>
+                        } 
+                    </li>
                     <li className="side-menu__item"><a href="">Home</a></li>
-                    <li className="side-menu__item"><a href="">Browse</a></li>
                     <li className="side-menu__item"><a href="">Start a petition</a></li>
                     <li className="side-menu__item"><a href="">About</a></li>
-                    <li className="side-menu__item"><a href="">Sign up</a></li>
-                    <li className="side-menu__item"><a href="">John Doe</a></li>
-                    <li className="side-menu__item"><a href="">Petitions</a></li>
                     <li className="side-menu__item"><a href="">Log out</a></li>
+                    
                 </ul>
             </div>
         </SwipeableDrawer>
